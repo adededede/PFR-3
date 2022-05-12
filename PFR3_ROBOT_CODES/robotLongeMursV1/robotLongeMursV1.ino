1ea3caf7d5c8af4c86f210d541771fa45dd55e6b
IgorVodkaBochkaBass <jloupgonzalez@gmail.com>
2022-05-12 11:14:44 +0200

    Update robotLongeMursV1.ino

--- PFR3_ROBOT_CODES/robotLongeMursV1/robotLongeMursV1.ino
+++ PFR3_ROBOT_CODES/robotLongeMursV1/robotLongeMursV1.ino
@@ -12,6 +12,7 @@ Servo sg, sd;
 float c1distance = 100;//on met par défaut une valeur non critique pour ne pas entrer
 float c2distance = 100;//dans un if dès la mise en marche du robot
 float c3distance = 30;
+boolean obstacle;
 
 //c1 : capteur avant
 //c2 : capteur avant gauche
@@ -45,19 +46,20 @@ void setup()
 }//fin setup
 
 void loop() {
-  
+
   //on déclenche les capteurs et on lit leurs valeurs en continu
   //tant qu'aucun capteur ne détecte une distance critique
   c1distance = lectureCapteurAvant();
   c2distance = lectureCapteurAvantGauche();
   c3distance = lectureCapteurLateral();
-  
+
   //affiche valeur distance dans terminal
   affichageDansLeTerminal(c1distance, c2distance, c3distance);
-  
+
   //arret d'urgence
-  if (c1distance < 60 && c1distance != 0 ) { //pour eviter les valeurs extremes en cas de non detection de mur
+  if (c1distance < 60 && c1distance > 5 ) { //pour eviter les valeurs extremes en cas de non detection de mur
     Serial.println("ARRET D'URGENCE ACTIVE");
+    obstacle = true;
     //retour sonore
     bip();
     //arret
@@ -65,35 +67,52 @@ void loop() {
     sd.writeMicroseconds(1500);
     delay(500);
     //tourne à droite pour éviter obstacle (tourne à 90° a droite)
-    sg.writeMicroseconds(1410);
-    sd.writeMicroseconds(1590);
-    delay(2200);
+    sg.writeMicroseconds(1300);
+    sd.writeMicroseconds(1600);
+    delay(500);
     //on remet le robot droit en marche avant
     sg.writeMicroseconds(1580);
     sd.writeMicroseconds(1580);
   }
 
   //pour longer le mur
-  else if ((c3distance < 30 && c3distance != 0) || (c2distance < 40 && c2distance != 0)) {
+  else if ((c3distance < 25 && c3distance != 0) || (c2distance < 45 && c2distance != 0)) {
     //retour sonore
     bip();
     //tourne a droite car trop prêt du mur
     //jusqu'à revenir dans le bon intervalle
     sd.writeMicroseconds(1680);
-    delay(300);
+    delay(150);
     //on remet le robot droit en marche avant
     sd.writeMicroseconds(1580);
   }
-  
+
   //pour longer le mur
-  else if (c3distance > 50) {
+  else if (c3distance > 40) {
     //retour sonore
     bip();
     //tourne a gauche car trop loin du mur
     sg.writeMicroseconds(1680);
-    delay(300);
+    delay(150);
     //on remet le robot droit en marche avant
     sg.writeMicroseconds(1580);
+  }
+
+  if (obstacle == true && (c3distance == 0 || c3distance >100)) {
+    obstacle = false;
+    //retour sonore
+    bip();
+    //arret
+    sg.writeMicroseconds(1500);
+    sd.writeMicroseconds(1500);
+    delay(600);
+    //tourne à droite pour relonger le du mur (tourne à 90° a gauche)
+    sd.writeMicroseconds(1300);
+    sg.writeMicroseconds(1600);
+    delay(500);
+    //on remet le robot droit en marche avant
+    sg.writeMicroseconds(1580);
+    sd.writeMicroseconds(1580);
   }
 }//fin loop
 
